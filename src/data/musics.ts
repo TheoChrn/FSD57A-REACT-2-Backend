91f2e5aa7bbba00d21014742a6a0056b3cf8a189
TheoChrn <theo.chrn@gmail.com>
2025-01-07 15:36:15 +0100

    musicById

---
+++ src/data/musics.ts
@@ -0,0 +1,32 @@
+export const musics = [
+  {
+    id: 1,
+    name: "One More Time",
+    author: "Daft Punk",
+    genre: "French House",
+  },
+  {
+    id: 2,
+    name: "Bohemian Rhapsody",
+    author: "Queen",
+    genre: "Rock",
+  },
+  {
+    id: 3,
+    name: "Smells Like Teen Spirit",
+    author: "Nirvana",
+    genre: "Grunge",
+  },
+  {
+    id: 4,
+    name: "Blinding Lights",
+    author: "The Weeknd",
+    genre: "Synthwave",
+  },
+  {
+    id: 5,
+    name: "Hotel California",
+    author: "Eagles",
+    genre: "Classic Rock",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
+import { musics } from "./data/musics";
 
-const app = express();
+const app: Application = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World with TypeScript and Express! LOl");
+app.get("/", (_, res: Response) => {
+  res.send("Hello World with TypeScript and Express!");
+});
+
+app.get("/musics", (_, res: Response) => {
+  res.json(musics);
+});
+
+app.get("/musics/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const musicById = musics.find((music) => music.id.toString() === id);
+
+    if (!musicById) {
+      res.status(404).json({ message: "Music not found" });
+      return;
+    }
+
+    res.status(200).json(musicById);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    return;
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default app;
